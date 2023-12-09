046208fd211136950e8a0f6512fbf1cd1db06f44
Hilliam Tung <hilliamtung@gmail.com>
2023-12-09 17:32:26 +0000

    Added AoriVault abi and types

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,11 +2,13 @@
 /* tslint:disable */
 /* eslint-disable */
 export type { AoriProtocol } from "./AoriProtocol";
+export type { AoriVault } from "./AoriVault";
 export type { BatchExecutor } from "./BatchExecutor";
 export type { ERC20 } from "./ERC20";
 export type { FlashExecutor } from "./FlashExecutor";
 export * as factories from "./factories";
 export { AoriProtocol__factory } from "./factories/AoriProtocol__factory";
+export { AoriVault__factory } from "./factories/AoriVault__factory";
 export { BatchExecutor__factory } from "./factories/BatchExecutor__factory";
 export { ERC20__factory } from "./factories/ERC20__factory";
 export { FlashExecutor__factory } from "./factories/FlashExecutor__factory";
