e9954bf7ff07784013e7dda7768c3f8866a3e525
Hilliam Tung <hilliamtung@gmail.com>
2023-12-21 19:03:50 +0000

    remove AoriSolutionStore for now

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,7 +1,6 @@
 export * from "./AoriDataProvider";
 export * from "./AoriHttpProvider";
 export * from "./AoriProvider";
-export * from "./AoriSolutionStore";
 export * from "./interfaces";
 export * from "./utils";
 
