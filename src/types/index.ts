dcf44effb6480d0e206165bc1261484ee769b32e
Hilliam Tung <hilliamtung@gmail.com>
2024-01-13 21:18:58 +0000

    added v2 abi

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,12 +2,14 @@
 /* tslint:disable */
 /* eslint-disable */
 export type { AoriProtocol } from "./AoriProtocol";
+export type { AoriV2 } from "./AoriV2";
 export type { AoriVault } from "./AoriVault";
 export type { BatchExecutor } from "./BatchExecutor";
 export type { ERC20 } from "./ERC20";
 export type { FlashExecutor } from "./FlashExecutor";
 export * as factories from "./factories";
 export { AoriProtocol__factory } from "./factories/AoriProtocol__factory";
+export { AoriV2__factory } from "./factories/AoriV2__factory";
 export { AoriVault__factory } from "./factories/AoriVault__factory";
 export { BatchExecutor__factory } from "./factories/BatchExecutor__factory";
 export { ERC20__factory } from "./factories/ERC20__factory";
