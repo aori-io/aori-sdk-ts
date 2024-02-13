dc1af7a791810b0338e3741f7007bafd4b8ee580
Hilliam Tung <hilliamtung@gmail.com>
2024-02-13 18:14:57 +0000

    added quoter types into sdk; removed adapters package

--- src/base/QuoteMaker.ts
+++ src/base/QuoteMaker.ts
@@ -1,8 +1,40 @@
-import { Quoter } from "@aori-io/adapters";
 import { Wallet } from "ethers";
 import { getFeeData } from "../providers";
 import { SubscriptionEvents } from "../utils";
 import { BaseMaker } from "./BaseMaker";
+
+/*//////////////////////////////////////////////////////////////
+                               TYPES
+//////////////////////////////////////////////////////////////*/
+
+export interface PriceRequest {
+    fromAddress: string;
+    inputToken: string;
+    outputToken: string;
+    inputAmount?: string;
+    outputAmount?: string;
+    chainId: number;
+}
+type WithRequired<T, K extends keyof T> = T & { [P in K]-?: T[P] }
+
+export type OutputAmountRequest = WithRequired<PriceRequest, "inputAmount">
+export type InputAmountRequest = WithRequired<PriceRequest, "outputAmount">
+
+export interface Calldata {
+    to: string;
+    value: number;
+    data: string;
+}
+export type Quote = {
+    outputAmount: bigint;
+    price: number;
+    gas: bigint;
+} & Calldata;
+export interface Quoter {
+    name: () => string;
+    getOutputAmountQuote: ({ inputToken, outputToken, inputAmount }: OutputAmountRequest) => Promise<Quote>;
+    getInputAmountQuote: ({ inputToken, outputToken, outputAmount }: InputAmountRequest) => Promise<Quote>;
+}
 
 export function QuoteMaker({
     wallet,
@@ -122,4 +154,3 @@ export function QuoteMaker({
 
     return baseMaker;
 }
-
