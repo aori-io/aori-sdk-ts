a039273b336599a64f8cc589e7c6c1724a3a6db2
Hilliam T <hilliamtung@gmail.com>
2024-05-27 23:28:21 +0100

    added verifyAuthMessage and signAuthMessage

---
+++ src/utils/signature.ts
@@ -0,0 +1,11 @@
+import { verifyMessage, Wallet } from "ethers";
+
+export function verifyAuthSignature(address: string, signature: string): boolean {
+    return address == verifyMessage(`${address}-01/${new Date().getMonth()}`, signature);
+}
+
+export function signAuthMessage(wallet: Wallet): string {
+    return wallet.signMessageSync(`${wallet.address}-01/${new Date().getMonth()}`);
+}
+
+// verifyAuthSignature(_wallet.address, signAuthMessage(_wallet));
