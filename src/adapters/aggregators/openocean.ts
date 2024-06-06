aa00266f763024004482300867e9f7fe24d8cc87
elstongun <75282682+elstongun@users.noreply.github.com>
2024-06-06 01:51:32 -0500

    Updated openocean

--- src/adapters/aggregators/openocean.ts
+++ src/adapters/aggregators/openocean.ts
@@ -3,42 +3,60 @@ import axios from "axios";
 
 export const OPENOCEAN_API_URL = "https://open-api.openocean.finance/v4";
 
+// Token whitelist array
+const tokenWhitelist = [
+    "0x4300000000000000000000000000000000000004", 
+    "0x4300000000000000000000000000000000000003", 
+];
+
 export class OpenOceanQuoter implements Quoter {
     url: string;
+    chain: string;
 
-    constructor({ url }: { url: string }) {
+    constructor({ url, chain }: { url: string; chain: string }) {
         this.url = url;
+        this.chain = chain;
     }
 
-    static build({ url }: { url: string; }) {
-        return new OpenOceanQuoter({ url });
+    static build({ url, chain }: { url: string; chain: string }) {
+        return new OpenOceanQuoter({ url, chain });
     }
 
     name() {
         return "openocean";
     }
 
-    async getOutputAmountQuote({ inputToken, outputToken, inputAmount, fromAddress, chainId }: PriceRequest) {
+    async getOutputAmountQuote({ inputToken, outputToken, inputAmount, fromAddress }: PriceRequest) {
+        const startTime = Date.now();
+        if (!tokenWhitelist.includes(inputToken) || !tokenWhitelist.includes(outputToken)) {
+            throw new Error("Input or output token is not on the whitelist");
+        }
+        const inputAmountInEther = Number(inputAmount) / 1e18;
 
-        const { data } = await axios.get(`${this.url}/${chainId}/swap`, {
+        const { data } = await axios.get(`${this.url}/${this.chain}/swap`, {
             params: {
                 inTokenAddress: inputToken,
                 outTokenAddress: outputToken,
-                amount: inputAmount,
-                account: fromAddress,
-                slippage: 2,
-                allowPartialFill: false,
-                gasPrice: 1
+                amount: inputAmountInEther,
+                gasPrice: 10,
+                slippage: 1,
+                account: fromAddress,                
             }
         });
 
+        const outputAmount = BigInt(data.data.outAmount);
+        const gasEstimate = 0n;
+
+        const endTime = Date.now();
+
+        console.log(`OpenOcean OutputAmount: ${outputAmount} Time: ${endTime - startTime}ms`);
         return {
             outputAmount: BigInt(data.data.outAmount),
             to: data.data.to,
             value: data.data.value,
             data: data.data.data,
             price: parseFloat(data.data.price),
-            gas: BigInt(1000000000n)
+            gas: BigInt(data.data.estimatedGas)
         };
     }
 
