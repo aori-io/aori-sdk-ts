98d49c806c4a9d60782194d5f4b88f868711525a
Hilliam T <hilliamtung@gmail.com>
2024-06-20 23:18:59 +0100

    allow for failed transactions to be sent on the last retry for evaluating

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -628,7 +628,7 @@ export async function sendOrRetryTransaction(wallet: Wallet, tx: TransactionRequ
             const signedTx = await wallet.signTransaction({ ...tx, nonce, gasPrice, ...(maxFeePerGas != null ? { maxFeePerGas, maxPriorityFeePerGas } : { gasLimit: 8_000_000n }) });
 
             // On first try, just send the transaction
-            await simulateTransaction(signedTx);
+            if (retries != 0) await simulateTransaction(signedTx);
             await sendTransaction(signedTx);
             success = true;
         } catch (e: any) {
