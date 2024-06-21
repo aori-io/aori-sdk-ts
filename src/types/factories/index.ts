5d13aa4c291aa6643ad3a57e55ec37ba38f0db49
Hilliam Tung <hilliamtung@gmail.com>
2024-06-21 11:52:57 +0100

    added blast variant with updated abi

--- src/types/factories/index.ts
+++ src/types/factories/index.ts
@@ -3,6 +3,7 @@
 /* eslint-disable */
 export { AoriV2__factory } from "./AoriV2__factory";
 export { AoriVault__factory } from "./AoriVault__factory";
+export { AoriVaultBlast__factory } from "./AoriVaultBlast__factory";
 export { CREATE3Factory__factory } from "./CREATE3Factory__factory";
 export { ERC20__factory } from "./ERC20__factory";
 export { Yang__factory } from "./Yang__factory";
--- src/types/index.ts
+++ src/types/index.ts
@@ -3,6 +3,7 @@
 /* eslint-disable */
 export type { AoriV2 } from "./AoriV2";
 export type { AoriVault } from "./AoriVault";
+export type { AoriVaultBlast } from "./AoriVaultBlast";
 export type { CREATE3Factory } from "./CREATE3Factory";
 export type { ERC20 } from "./ERC20";
 export type { Yang } from "./Yang";
@@ -10,6 +11,7 @@ export type { Yin } from "./Yin";
 export * as factories from "./factories";
 export { AoriV2__factory } from "./factories/AoriV2__factory";
 export { AoriVault__factory } from "./factories/AoriVault__factory";
+export { AoriVaultBlast__factory } from "./factories/AoriVaultBlast__factory";
 export { CREATE3Factory__factory } from "./factories/CREATE3Factory__factory";
 export { ERC20__factory } from "./factories/ERC20__factory";
 export { Yang__factory } from "./factories/Yang__factory";
