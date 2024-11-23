9d4287262850edc1faac6259e2e36c9fbb66f508
Hilliam T <hilliamtung@gmail.com>
2024-11-23 01:54:21 +0000

    added verifyOrderSignature

--- src/utils/signature.ts
+++ src/utils/signature.ts
@@ -1,8 +1,12 @@
-import { getBytes, solidityPackedKeccak256, Wallet } from "ethers";
+import { getBytes, solidityPackedKeccak256, verifyMessage, Wallet } from "ethers";
 import { AoriOrder, SignedOrder } from "./interfaces";
 import { getOrderHash } from "../providers";
 
-export function getSignatureMessage(order: SignedOrder) {
+/*//////////////////////////////////////////////////////////////
+                        ORDER SIGNATURE
+//////////////////////////////////////////////////////////////*/
+
+export function getOrderMessage(order: SignedOrder) {
     return solidityPackedKeccak256(
         ["bytes32", "bytes"],
         [
@@ -12,20 +16,23 @@ export function getSignatureMessage(order: SignedOrder) {
     )
 }
 
-export function signOrderWithExtra(wallet: Wallet, order: AoriOrder, extraData: string = "0x"): SignedOrder {
-    const signature = wallet.signMessageSync(
-        getBytes(getSignatureMessage({ order, extraData, signature: "" }))
-    );
-
-    return {
-        order,
-        extraData,
-        signature,
-    }
+export async function signOrderWithExtradata(wallet: Wallet, order: AoriOrder, extraData: string = "0x") {
+    return wallet.signMessageSync(getBytes(getOrderMessage({ order, extraData, signature: "" })));
 }
 
+export function verifyOrderSignature(signedOrder: SignedOrder): boolean {
+    return verifyMessage(
+        getBytes(getOrderMessage(signedOrder)),
+        signedOrder.signature
+    ) == signedOrder.order.offerer;
+}
+
+/*//////////////////////////////////////////////////////////////
+                        SEQUENCE SIGNATURE
+//////////////////////////////////////////////////////////////*/
+
 export function getSequenceMessage(orders: SignedOrder[], extraData: string) {
-    const messages = orders.map(order => getSignatureMessage(order));
+    const messages = orders.map(order => getOrderMessage(order));
     return solidityPackedKeccak256(
         ["bytes32[]", "bytes"],
         [
