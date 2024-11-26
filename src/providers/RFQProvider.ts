45fb564678c9e39c71329b62d1c3c72df86820b5
kev-daniell <kevindkevdan@gmail.com>
2024-11-25 19:23:11 -0500

    full impl of core action wrappers

--- src/providers/RFQProvider.ts
+++ src/providers/RFQProvider.ts
@@ -1,5 +1,5 @@
 import { WebSocket } from "ws";
-import { AORI_HTTP_API, AORI_QUOTER_API, AORI_WS_API, AoriDataServerMethods, AoriMethods, AoriOrder, AoriQuoterMethods, AoriWebsocketEventData, rawCall, SubscriptionEvents, TypedEventEmitter } from "../utils";
+import { AORI_HTTP_API, AORI_QUOTER_API, AORI_WS_API, AoriMethods, AoriOrder, AoriQuoterMethods, AoriWebsocketEventData, OrderCancelledDetails, QuoteRequestedDetails, rawCall, SubscriptionEvents, TradeMatchedDetails, TypedEventEmitter, WithEventDetails } from "../utils";
 
 export interface AoriSubscribeParams {
     tradeId?: string;
@@ -108,43 +108,99 @@ export class RFQProvider extends TypedEventEmitter<AoriWebsocketEventData> {
                             HELPERS
 //////////////////////////////////////////////////////////////*/
 
+interface AoriGetPriceEstimateParams {
+    inputToken: string;
+    outputToken: string;
+    inputAmount: number;
+    chainId: number;
+}
+
 // getPriceEstimate
-export async function getPriceEstimate(inputToken: string, outputToken: string, inputAmount: number, chainId: number) {
-    return rawCall(AORI_QUOTER_API, AoriQuoterMethods.PriceQuote, [{
-        inputToken,
-        outputToken,
-        inputAmount,
-        chainId
+export async function getPriceEstimate(params: AoriGetPriceEstimateParams) {
+    return await rawCall(AORI_QUOTER_API, AoriQuoterMethods.PriceQuote, [{
+        inputToken: params.inputToken,
+        outputToken: params.outputToken,
+        inputAmount: params.inputAmount,
+        chainId: params.chainId
     }]);
 }
 
-// requestPartialQuote TODO
-export async function requestPartialQuote() {
 
+interface AoriGetPartialQuoteParams {
+    inputToken: string;
+    outputToken: string;
+    inputAmount: number;
+    chainId: number;
+}
+
+// requestPartialQuote
+export async function requestPartialQuote(params: AoriGetPartialQuoteParams) {
+    return await rawCall(AORI_HTTP_API, AoriMethods.Rfq, [{
+        inputToken: params.inputToken,
+        outputToken: params.outputToken,
+        inputAmount: params.inputAmount,
+        chainId: params.chainId
+    }]);
+}
+
+interface AoriRequestQuoteParams {
+    order: AoriOrder;
+    signature: string;
 }
 
 // requestQuote
-export async function requestQuote(order: AoriOrder, signature: string) {
-    return rawCall(AORI_HTTP_API, AoriMethods.Rfq, [{ order, signature }]);
+export async function requestQuote(params: AoriRequestQuoteParams) {
+    return await rawCall(AORI_HTTP_API, AoriMethods.Rfq, [{
+        order: params.order,
+        signature: params.signature
+    }]);
 }
 
-// respondToQuote TODO 
-export async function respondToQuote() {
-
+interface AoriRespondToQuoteParams {
+    tradeId: string;
+    order: AoriOrder;
+    signature: string;
+}
+// respondToQuote  
+export async function respondToQuote(params: AoriRespondToQuoteParams) {
+    return await rawCall(AORI_HTTP_API, AoriMethods.Respond, [{
+        tradeId: params.tradeId,
+        order: params.order,
+        signature: params.signature
+    }]);
 }
 
-// makeOrder TODO
-export async function makeOrder(order: AoriOrder, signature: string) {
-    return rawCall(AORI_HTTP_API, AoriMethods.Make, [{ order, signature }]);
+interface AoriMakeOrderParams {
+    order: AoriOrder;
+    signature: string;
+    feeTag?: string;
+    feeRecipient?: string;
+    feeInBips?: number;
+    isPrivate?: boolean
 }
 
-// takeOrder TODO 
-export async function takeOrder() {
-
+// makeOrder 
+export async function makeOrder(params: AoriMakeOrderParams) {
+    return await rawCall(AORI_HTTP_API, AoriMethods.Make, [{
+        order: params.order,
+        feeTag: params.feeTag,
+        feeRecipient: params.feeRecipient,
+        feeInBips: params.feeInBips,
+        isPrivate: params.isPrivate,
+        signature: params.signature
+    }]);
 }
 
-// cancelOrder TODO
-export async function cancelOrder(orderHash: string, signature: string) {
-    return rawCall(AORI_HTTP_API, AoriMethods.Cancel, [{ orderHash, signature }]);
 
+interface AoriCancelParams {
+    tradeId: string;
+    signature: string;
+}
+
+// cancelOrder
+export async function cancelOrder(params: AoriCancelParams) {
+    return await rawCall(AORI_HTTP_API, AoriMethods.Cancel, [{
+        tradeId: params.tradeId,
+        signature: params.signature
+    }]);
 }
